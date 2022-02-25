27450e13a9ceb8c827f424f19fc6a9a468f30822
John Lunz <johnlunz8@gmail.com>
2022-02-24 20:49:00 -0500

    Added Basic JavaScript to grab timer variable elem

---
+++ stopWatch/script.js
@@ -0,0 +1,10 @@
+// create variable of seconds passed
+let milliseconds = 3800000;
+
+// create function to format seconds
+
+
+// get timer element
+timer = document.querySelector("#timer-variable");
+console.log(timer);
+timer.innerHTML = milliseconds;
