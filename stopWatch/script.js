172adb3c2184c87490a858be5ae57d79004bfc38
John Lunz <johnlunz8@gmail.com>
2022-02-24 21:06:21 -0500

    Created FormatTime JavaScript Function

--- stopWatch/script.js
+++ stopWatch/script.js
@@ -1,10 +1,35 @@
-// create variable of seconds passed
-let milliseconds = 3800000;
+// create variables of time passed
+let milliseconds = 0;
+let hours = 0
+let minutes = 0
+let seconds = 0
 
 // create function to format seconds
+const formatTimeElapsed = function(amtOfTime, hours, minutes, seconds) {
+    const numMillisecondsPerHour = 3600000;
+    const numMillisecondsPerMinute = 60000;
+    const numMillisecondsPerSecond = 1000;
 
+    while (amtOfTime / numMillisecondsPerHour >= 1) {
+        hours += 1;
+        amtOfTime -= numMillisecondsPerHour;
+    }
+    while (amtOfTime / numMillisecondsPerMinute >= 1) {
+        minutes += 1;
+        amtOfTime -= numMillisecondsPerMinute;
+    }
+    while (amtOfTime / numMillisecondsPerSecond >= 1) {
+        seconds += 1;
+        amtOfTime -= numMillisecondsPerSecond;
+    }
+    return {
+        'hours':hours,
+        'minutes':minutes,
+        'seconds':seconds,
+        'milliseconds':amtOfTime
+    }
+}
 
 // get timer element
-timer = document.querySelector("#timer-variable");
-console.log(timer);
-timer.innerHTML = milliseconds;
+
+console.log(formatTimeElapsed(65013, hours, minutes, seconds))
